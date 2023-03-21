c82658747e998c634bf81007791f08af8abee744
dnitecki <dominick.nitecki@outlook.com>
2023-03-21 00:21:36 -0400

    add more topbar functiona9lity

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -2,11 +2,5 @@ import React from "react";
 import "./SideBar.scss";
 
 export default function SideBar() {
-  return (
-    <>
-      <div className="sidebar-container">
-        <div className="sidebar-content"></div>
-      </div>
-    </>
-  );
+  return <></>;
 }
