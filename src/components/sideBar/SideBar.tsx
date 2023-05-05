b65a4e6cd3879cdfbc93ef46f17fa385b1519120
dnitecki <dominick.nitecki@outlook.com>
2023-05-05 00:44:33 -0400

    add kinesys icon

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,43 +1,42 @@
 import React from "react";
 import "./SideBar.scss";
-import {
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { List, ListItem, ListItemButton, ListItemIcon } from "@mui/material";
 import { Link } from "react-router-dom";
 import { menuItems } from "./menuItems";
+import KinesysIcon from "../../icons/KinesysIcon.svg";
 
 export default function SideBar() {
   return (
-    <div className="sidebar-container">
-      <List>
-        {menuItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: "block" }}>
-            <ListItemButton
-              component={Link}
-              to={item.url}
-              sx={{
-                width: "auto",
-                alignContent: "center",
-                justifyContent: "center",
-              }}
-            >
-              <ListItemIcon
+    <>
+      <div className="sidebar-container">
+        <div className="sidebar-icon-container">
+          <img className="sidebar-icon" src={KinesysIcon} alt="Kinesys Icon" />
+        </div>
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem key={index} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                component={Link}
+                to={item.url}
                 sx={{
-                  minWidth: 0,
+                  width: "auto",
+                  alignContent: "center",
                   justifyContent: "center",
                 }}
               >
-                {<item.icon />}
-              </ListItemIcon>
-              {/* <ListItemText primary={item.text} /> */}
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    justifyContent: "center",
+                  }}
+                >
+                  {<item.icon />}
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </>
   );
 }
