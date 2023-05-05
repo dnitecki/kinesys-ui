ca0fff30c8a612f29e749c56e9996520f9bb2f39
dnitecki <dominick.nitecki@outlook.com>
2023-05-05 17:23:23 -0400

    refactor sidebar

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./SideBar.scss";
-import { List, ListItem, ListItemButton, ListItemIcon } from "@mui/material";
 import { Link } from "react-router-dom";
 import { menuItems } from "./menuItems";
 import KinesysIcon from "../../icons/KinesysIcon.svg";
@@ -12,32 +11,17 @@ export default function SideBar() {
         <div className="sidebar-icon-container">
           <img className="sidebar-icon" src={KinesysIcon} alt="Kinesys Icon" />
         </div>
-        <List>
+        <ul className="sidebar-list">
           {menuItems.map((item, index) => (
-            <ListItem key={index} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                component={Link}
-                to={item.url}
-                sx={{
-                  width: "auto",
-                  alignContent: "center",
-                  justifyContent: "center",
-                  color: "#4D4D4D",
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    justifyContent: "center",
-                    color: "#4D4D4D",
-                  }}
-                >
-                  {<item.icon />}
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
+            <li className="sidebar-item" key={index}>
+              <Link className="sidebar-button" to={item.url}>
+                <div className="sidebar-item-icon">
+                  {<item.icon sx={{ fontSize: 30 }} />}
+                </div>
+              </Link>
+            </li>
           ))}
-        </List>
+        </ul>
       </div>
     </>
   );
