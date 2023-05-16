36afb1e27166a56ffed43e4dae322a87fa29f5c2
dnitecki <dominick.nitecki@outlook.com>
2023-05-16 18:26:10 -0400

    add link

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Dashboard.scss";
 import ChartCard from "../../components/chartCard/ChartCard";
+import EastRoundedIcon from "@mui/icons-material/EastRounded";
 
 export default function Dashboard() {
   return (
     <>
       <div className="page-container">
+        <section className="page-heading-link">
+          <a href="market">View Pipeline Date</a>
+          <EastRoundedIcon />
+        </section>
         <section className="page-tiles">
           <ChartCard />
         </section>
