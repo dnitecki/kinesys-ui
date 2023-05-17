2b23b3a4059f6d806fd7d0b8a78a57e12a9bf46a
dnitecki <dominick.nitecki@outlook.com>
2023-05-17 13:01:04 -0400

    remove mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import "./App.scss";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./utils/themes/theme";
 import AppContainer from "./components/appContainer/AppContainer";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <div className="app">
-          <div className="app-container">
-            <Routes>
-              <Route path="/*" element={<AppContainer />} />
-            </Routes>
-          </div>
+      <div className="app">
+        <div className="app-container">
+          <Routes>
+            <Route path="/*" element={<AppContainer />} />
+          </Routes>
         </div>
-      </ThemeProvider>
+      </div>
     </>
   );
 }
