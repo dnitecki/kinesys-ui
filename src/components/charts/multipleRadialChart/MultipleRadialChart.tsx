640f8658c23e018c9cf2e2cd6392cbfb63b58ab5
dnitecki <dominick.nitecki@outlook.com>
2023-05-28 23:08:05 -0400

    add radial chart

--- src/components/charts/multipleRadialChart/MultipleRadialChart.tsx
+++ src/components/charts/multipleRadialChart/MultipleRadialChart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
+import "./MultipleRadialChart.scss";
 
 export default function MultipleRadialChart({ ...data }) {
   const chartOptions: ApexOptions = {
@@ -41,18 +42,39 @@ export default function MultipleRadialChart({ ...data }) {
     stroke: {
       lineCap: "round",
     },
+    fill: {
+      colors: ["#f3a225", "#4dbde8"],
+    },
   };
 
   return (
     <>
-      <div className="chart-card-title">{data.title}</div>
-      <ReactApexChart
-        options={chartOptions}
-        series={chartOptions.series}
-        type="radialBar"
-        height={200}
-        width={150}
-      />
+      <div className="multiRad-container">
+        <div className="multiRad-chart">
+          <ReactApexChart
+            options={chartOptions}
+            series={chartOptions.series}
+            type="radialBar"
+            height={200}
+            width={150}
+          />
+        </div>
+        <div className="multiRad-legend">
+          <div className="chart-card-title">{data.title}</div>
+          <div className="multiRad-bar-container">
+            <div className="multiRad-bar bar-sales">Sales</div>
+            <div className="sales-number">
+              {data.multipleRadial.values.series[0]}
+            </div>
+          </div>
+          <div className="multiRad-bar-container">
+            <div className="multiRad-bar bar-clients">Clients</div>
+            <div className="client-number">
+              {data.multipleRadial.values.series[1]}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
