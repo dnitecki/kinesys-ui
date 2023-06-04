4f4e6821903bb306604309a7e7fe107bde8b7b3c
dnitecki <dominick.nitecki@outlook.com>
2023-06-04 18:56:39 -0400

    update

--- src/pages/dashboard/Tabs/Status/Status.tsx
+++ src/pages/dashboard/Tabs/Status/Status.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-import ChartCard from "../../../../components/chartCard/ChartCard";
 
-export default function Status({ ...data }: any) {
-  return (
-    <div>
-      <section className="page-tiles">
-        {data
-          ? data?.map((item: any, index: number) => (
-              <ChartCard {...item} key={index} />
-            ))
-          : null}
-      </section>
-    </div>
-  );
+export default function Status() {
+  return <div></div>;
 }
