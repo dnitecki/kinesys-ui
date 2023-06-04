cd0e5c8c1baef51063d15f9ff7f1de5c3ebce2e5
dnitecki <dominick.nitecki@outlook.com>
2023-06-03 22:11:45 -0400

    add react-query

---
+++ src/services/constants/serviceConstants.ts
@@ -0,0 +1,2 @@
+export const API_URL =
+  "https://4bf93eda-989d-46a4-8aa0-18b1b3ff20c3.mock.pstmn.io";
---
+++ src/services/dashboardService.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { API_URL } from "./constants/serviceConstants";
+
+const getStatusService = async () => {
+  const result = await axios.get(`${API_URL}/status`);
+  return result.data;
+};
+
+export const getStatus = () => {
+  const { isLoading, error, data } = useQuery("status", getStatusService);
+  return { isLoading, error, data };
+};
