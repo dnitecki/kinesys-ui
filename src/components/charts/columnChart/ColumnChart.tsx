b0b0797732a5326ce8b1b3b8bb3100cb633989fe
dnitecki <dominick.nitecki@outlook.com>
2023-06-07 00:02:15 -0400

    format data

--- src/components/charts/columnChart/ColumnChart.tsx
+++ src/components/charts/columnChart/ColumnChart.tsx
@@ -4,21 +4,21 @@ import ReactApexChart from "react-apexcharts";
 import { chartFillColorByType } from "../../../utils/helpers/helperFunctions";
 
 export default function ColumnChart({ ...data }: any) {
+  const formatData = () => {
+    const keys = ["data"];
+    const series = data.columnChart.values.map(
+      (element: { series: number[] }) => element.series
+    );
+    const seriesObj = keys.reduce(
+      (obj, key, index) => ({ ...obj, [key]: series[index] }),
+      {}
+    );
+    console.log(seriesObj);
+    return [seriesObj];
+  };
+  formatData();
   const chartOptions: ApexOptions = {
-    series: [
-      {
-        name: "Net Profit",
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-      },
-      {
-        name: "Revenue",
-        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-      },
-      {
-        name: "Free Cash Flow",
-        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-      },
-    ],
+    series: [],
     chart: {
       height: 350,
       width: "100%",
